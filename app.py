2db8ad318ccd765a344fb6aa5342a2f06cead0e3
Laura Zaragoza <laurazaragoza@Lauras-MacBook-Pro.local>
2021-07-28 00:49:49 -0700

    update py file

--- app.py
+++ app.py
@@ -1,34 +1,34 @@
-# 1. import Flask
 import numpy as np
+
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+from app import app
 
 from flask import Flask, jsonify
+
 
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///titanic.sqlite")
+
+engine = create_engine("sqlite:///hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
+
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
-# Save reference to the table
-Passenger = Base.classes.passenger
-
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 #################################################
 # Flask Setup
 #################################################
 
-
 app = Flask(__name__)
-
 
 #################################################
 # Flask Routes
@@ -36,42 +36,31 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
+    """List all available api routes."""
     return (
-        f"Welcome to the Justice League API!<br/>"
         f"Available Routes:<br/>"
-        f"/api/v1.0/justice-league"
-
-@app.route("/api/v1.0/precipitation")
-def justice_league():
-    """Return the justice league data as json"""
-
-    return jsonify(justice_league_members)
+        f"/api/v1.0/precipitation"
+        f"/api/v1.0/stations"
+        f"/api/v1.0/tobs"
+        f"/api/v1.0/<start>")
+        # f"/api/v1.0/<start>/<end>")
 
 @app.route("/api/v1.0/stations")
-def welcome():
-    return (
-        f"Welcome to the Justice League API!<br/>"
-        f"Available Routes:<br/>"
-        f"/api/v1.0/justice-league"
-    )
-
-@app.route("/api/v1.0/tobs")
-def names():
-    # Create our session (link) from Python to the DB
+def stations():
+    """Return the list of stations from the dataset as json"""
     session = Session(engine)
-
-    """Return a list of all passenger names"""
-    # Query all passengers
-    results = session.query(Passenger.name).all()
-
+    results = session.query(Measurement.date, Measurement.prcp).all()
     session.close()
+    return jsonify(stations)
 
-    # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
+# @app.route("/api/v1.0/precipitation")
+# def prcp():
+#     """Return the justice league data as json"""
+#     return()
+#     # Convert the query results to a dictionary using date as the key and prcp as the value.
+#     # Return the JSON representation of your dictionary.
 
-    return jsonify(all_names)
 
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
